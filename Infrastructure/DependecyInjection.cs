8e3820fe1077b7ce1d5c635a3b04774b30f280ae
Danis <deanis89@hotmail.com>
2023-08-14 16:00:41 -0500

    Refactor(Dependency Injection)
    Add Method for infraestructure on dev enviroment

--- Infrastructure/DependecyInjection.cs
+++ Infrastructure/DependecyInjection.cs
@@ -6,29 +6,51 @@ using sureApp.Infrastructure.CoverageSource;
 using sureApp.domain.CoverageEntity;
 using Infrastructure.Helpers;
 using Infrastructure.CoverageSource;
+using Infrastructure.CoverageFeatureSource;
 
 namespace sureApp.Infrastructure
 {
     public static class DependecyInjection
     {
-        public static async Task<IServiceCollection> AddInfrastructure(this
+        public static bool IsEnviromentDev = true;
+        private static CreateAutoincrementalEntitys autoIndexDb;
+        private static ApplicationDbContext dbContext;
+
+        public static async Task<IServiceCollection> AddInfraestructureInDevMode(this
             IServiceCollection services, IConfiguration configuration)
         {
+            IsEnviromentDev = configuration["Enviroment"] != "production";
+            if (IsEnviromentDev)
+            {
+                var autoIndexDb = new CreateAutoincrementalEntitys(dbContext);
+                await Configurator.ExecutePreInitializator(dbContext, autoIndexDb);
+            }
+            return services;
+
+        }
+            public static IServiceCollection AddInfrastructure(this
+            IServiceCollection services, IConfiguration configuration)
+        {
+            Configurator.AddObject(new CoverageFeatureConfig());
             Configurator.AddObject(new CoverageConfigs());
             Configurator.ExecuteMapping();
 
             var connectionString = configuration.GetConnectionString("MongoDbConnectionString");
             var databaseName = configuration.GetConnectionString("MongoDatabaseName");
-            var dbContext = new ApplicationDbContext(connectionString, databaseName);
+           
             if (connectionString is null || databaseName is null)
             {
                 throw new ArgumentNullException(nameof(connectionString));
             }
+            dbContext = new ApplicationDbContext(connectionString, databaseName);
             _ = services.AddScoped(
                   provider => dbContext);
             services.AddScoped<ICoverageRepository, CoverageRepository>();
             services.AddScoped<ICoverageServices, CoverageServices>();
-            await Configurator.ExecutePreInitializator(dbContext);
+
+            autoIndexDb = new CreateAutoincrementalEntitys(dbContext);
+            services.AddSingleton(provider => autoIndexDb);
+            
             return services;
         }
 
