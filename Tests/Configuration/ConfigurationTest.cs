a50e683a536b5670a56fdbc05aad71a2ee78fde9
Danis <deanis89@hotmail.com>
2023-08-15 06:27:31 -0500

    FEAT(Tests)

---
+++ Tests/Configuration/ConfigurationTest.cs
@@ -0,0 +1,31 @@
+﻿using API.Services.Auth;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Configuration
+{
+    public class ConfigurationTest
+    {
+
+        [Fact]
+        public void LoadConfigurationJWT_Success()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            var Jwt = new Jwt();
+            configuration.Bind("Jwt", Jwt);
+
+            Assert.NotNull(Jwt.Key);
+            Assert.NotNull(Jwt.Subject);
+            Assert.NotNull(Jwt.Audience);
+            Assert.NotNull(Jwt.Issuer);
+        }
+    }
+}
---
+++ Tests/Controllers/Auth/LoginTest.cs
@@ -0,0 +1,51 @@
+﻿using API.Controllers;
+using API.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Tests.Controllers.Auth
+{
+    public class LoginTest
+    {
+        private readonly AuthenticationServices _auth;
+        private readonly AuthController _authController;
+
+        public LoginTest()
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                {"Jwt:Key", "TEST_Aplication-TEStuNA BUENA KEY"},
+                {"Jwt:Issuer", "https://localhost:7183/"},
+                {"Jwt:Audience", "https://localhost:7183/"},
+                {"Jwt:Subject", "test"}
+            })
+            .Build();
+            _auth = new AuthenticationServices(configuration);
+            _authController = new AuthController(_auth);
+        }
+        [Fact]
+        public void Login_OK()
+        {
+            var user = new Login
+            {
+                Email = "admin@admin.co",
+                Password = "1234"
+            };
+            var result = _authController.Login(user);
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void Login_Bad()
+        {
+            var user = new Login
+            {
+                Email = "admin@admin.cow",
+                Password = "1234"
+            };
+            var result = _authController.Login(user);
+            Assert.IsType<UnauthorizedResult>(result);
+        }
+    }
+}
