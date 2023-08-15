75ae2ae07344bebdd7c3845ffd5c3b3ab2cc2ee1
Danis <deanis89@hotmail.com>
2023-08-15 00:49:41 -0500

    REFACTOR(IGetByCustomDataAsync)

--- Domain/Interfaces/Behaviors/IGetByCustomDataAsync.cs
+++ Domain/Interfaces/Behaviors/IGetByCustomDataAsync.cs
@@ -1,6 +1,6 @@
 ﻿namespace sureApp.domain.Interfaces.Behaviors
 {
-    internal interface IGetByCustomDataAsyncc<Entity, EntityCustomSearcherData> where Entity : class
+    public interface IGetByCustomDataAsync<Entity, EntityCustomSearcherData> where Entity : class
     {
         Task<Entity> GetByCustomDataSearcher(EntityCustomSearcherData data);
     }
