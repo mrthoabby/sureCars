2cacc4c2c7c4df26131cc026dd31cace7f3bd0b5
Danis <deanis89@hotmail.com>
2023-08-14 21:30:17 -0500

    REFACTOR(ApplicationDbContext)

--- Infrastructure/ApplicationDbContext.cs
+++ Infrastructure/ApplicationDbContext.cs
@@ -1,24 +1,32 @@
 ﻿using Infrastructure.Helpers;
 using MongoDB.Driver;
-using sureApp.domain.CoverageEntity;
 using sureApp.domain.CoverageFeatureEntity;
+using Domain.InsurancePolicyEntity;
+using sureApp.domain.CustomerEntity;
+using Domain.ContractEntity;
+using MongoDB.Bson;
 
 namespace sureApp.Infrastructure
 {
     public class ApplicationDbContext
     {
-        public IMongoCollection<Coverage> Coverages { get; private init; }
+        public IMongoCollection<InsurancePolicy> Coverages { get; private init; }
         public IMongoCollection<CoverageFeature> CoverageFeatures { get; private init; }
+        public IMongoCollection<Customer> Customers { get; private init; }
+        public IMongoCollection<Contract> Contracts { get; private init; }
         public IMongoCollection<CounterAutoIncremental> Counters { get; private init; }
+        
 
         private readonly IMongoDatabase database;
         public ApplicationDbContext(string connectionString,string databaseName)
         {
             var client = new MongoClient(connectionString);
             database = client.GetDatabase(databaseName);
-            Coverages = database.GetCollection<Coverage>(nameof(Coverage));
+            Coverages = database.GetCollection<InsurancePolicy>(nameof(InsurancePolicy));
             Counters = database.GetCollection<CounterAutoIncremental>(nameof(CounterAutoIncremental));
             CoverageFeatures = database.GetCollection<CoverageFeature>(nameof(CoverageFeature));
+            Customers = database.GetCollection<Customer>(nameof(Customer));
+            Contracts = database.GetCollection<Contract>(nameof(Contract));
         }
     }
 }
