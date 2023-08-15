af55ac96700b47c67440d80f4efc499887c980e2
Danis <deanis89@hotmail.com>
2023-08-14 21:28:25 -0500

    FEACT(ResponseResult)

---
+++ Application/Wrappers/ResponseResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Wrappers
+{
+    //El recomendado para tener responses uniformes
+    public record ResponseResult<DataResponse>(bool IsSusses,string? message,DataResponse? result, object?[]errors)
+    {
+    }
+}
